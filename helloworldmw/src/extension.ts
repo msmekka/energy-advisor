32ca6f635bae679fcebb41a6d777d15731bafd5f
msmekka <mekwil74@gmail.com>
2026-08-12 11:27:08 -0400

    Day 2 commit

--- helloworldmw/src/extension.ts
+++ helloworldmw/src/extension.ts
@@ -13,13 +13,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('helloworldmw.helloWorld', () => {
+	const hwdisposable = vscode.commands.registerCommand('helloworldmw.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello VS Code');
+
 	});
 
-	context.subscriptions.push(disposable);
+	const dtdisposable = vscode.commands.registerCommand('hellowworldmw.displayTime', () => {
+		const readable = new Date(Date.now())
+		vscode.window.showInformationMessage('Today\'s date is %d', readable.toLocaleString())
+	});
+
+	context.subscriptions.push(hwdisposable, dtdisposable);
 }
 
 // This method is called when your extension is deactivated
